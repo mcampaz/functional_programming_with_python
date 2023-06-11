aec606c8c54eb8f82ea688f5489108a1df76d31c
MARIO CAMPAZ <mcampaz@users.noreply.github.com>
2023-06-10 23:26:08 -0700

    When to use comprehensive list

--- when_how/create_lists.py
+++ when_how/create_lists.py
@@ -1,39 +1,55 @@
-If you want to create a list containing the first ten perfect squares, then you can complete these steps in three lines of code:
+#   If you want to create a list containing the first ten perfect squares, then you can complete these steps in three lines of code:
 
->>> squares = []
->>> for i in range(10):
-...     squares.append(i * i)
->>> squares
-[0, 1, 4, 9, 16, 25, 36, 49, 64, 81]
+squares = []
+for i in range(10):
+    squares.append(i * i)
+print(squares)
+# [0, 1, 4, 9, 16, 25, 36, 49, 64, 81]
 
+#
+# # consider a situation in which you need to calculate the price after tax for a list of transactions:
+#
 
-consider a situation in which you need to calculate the price after tax for a list of transactions:
-
->>>
->>> txns = [1.09, 23.56, 57.84, 4.56, 6.78]
->>> TAX_RATE = .08
->>> def get_price_with_tax(txn):
-...     return txn * (1 + TAX_RATE)
->>> final_prices = map(get_price_with_tax, txns)
->>> list(final_prices)
-[1.1772000000000002, 25.4448, 62.467200000000005, 4.9248, 7.322400000000001]
+txns = [1.09, 23.56, 57.84, 4.56, 6.78]
+TAX_RATE = .08
 
 
-you could rewrite the for loop from the first example in just a single line of code:
-
->>>
->>> squares = [i * i for i in range(10)]
->>> squares
-[0, 1, 4, 9, 16, 25, 36, 49, 64, 81]
+def get_price_with_tax(txn):
+    return txn * (1 + TAX_RATE)
 
 
-You can rewrite the pricing example with its own list comprehension:
+final_prices = map(get_price_with_tax, txns)
+print(list(final_prices))
+# # [1.1772000000000002, 25.4448, 62.467200000000005, 4.9248, 7.322400000000001]
+#
+#
+# # you could rewrite the for loop from the first example in just a single line of code:
+#
+#
+squares = [i * i for i in range(10)]
+print(squares)
+# # [0, 1, 4, 9, 16, 25, 36, 49, 64, 81]
+#
+#
+# # You can rewrite the pricing example with its own list comprehension:
+#
+#
+# txns = [1.09, 23.56, 57.84, 4.56, 6.78]
+# TAX_RATE = .08
+#
+#
+# def get_price_with_tax(txn):
+#     return txn * (1 + TAX_RATE)
+#
 
->>>
->>> txns = [1.09, 23.56, 57.84, 4.56, 6.78]
->>> TAX_RATE = .08
->>> def get_price_with_tax(txn):
-...     return txn * (1 + TAX_RATE)
->>> final_prices = [get_price_with_tax(i) for i in txns]
->>> final_prices
-[1.1772000000000002, 25.4448, 62.467200000000005, 4.9248, 7.322400000000001]
+final_prices2 = [get_price_with_tax(i) for i in txns]
+print(final_prices2)
+# # [1.1772000000000002, 25.4448, 62.467200000000005, 4.9248, 7.322400000000001]
+#
+#
+# print("functiomal:", get_price_with_tax)
+#
+# objects = ["functional", get_price_with_tax, 23]
+# print(objects[1](9))
+# final_prices = [objects[1](i) for i in txns]
+# print(final_prices)
--- when_how/when_not_to_use_list_comprehension.py
+++ when_how/when_not_to_use_list_comprehension.py
@@ -1,109 +1,129 @@
-The perfect data structure for storing this data could be a Python list comprehension nested within a dictionary comprehension:
+# The perfect data structure for storing this data could be
+# a Python list comprehension nested within a dictionary comprehension:
+from pprint import pprint
+import random
+import timeit
+#
+# cities = ['Austin', 'Tacoma', 'Topeka', 'Sacramento', 'Charlotte']
+# temps = {city: [0 for _ in range(7)] for city in cities}
+# pprint(f"Cities in set: {temps}")
 
->>>
->>> cities = ['Austin', 'Tacoma', 'Topeka', 'Sacramento', 'Charlotte']
->>> temps = {city: [0 for _ in range(7)] for city in cities}
->>> temps
-{
-    'Austin': [0, 0, 0, 0, 0, 0, 0],
-    'Tacoma': [0, 0, 0, 0, 0, 0, 0],
-    'Topeka': [0, 0, 0, 0, 0, 0, 0],
-    'Sacramento': [0, 0, 0, 0, 0, 0, 0],
-    'Charlotte': [0, 0, 0, 0, 0, 0, 0]
-}
+# {
+#     'Austin': [0, 0, 0, 0, 0, 0, 0],
+#     'Tacoma': [0, 0, 0, 0, 0, 0, 0],
+#     'Topeka': [0, 0, 0, 0, 0, 0, 0],
+#     'Sacramento': [0, 0, 0, 0, 0, 0, 0],
+#     'Charlotte': [0, 0, 0, 0, 0, 0, 0]
+# }
 
+#
+# Nested lists are a common way to create matrices,
+# which are often used for mathematical purposes.
+# Take a look at the code block below:
+#
 
-Nested lists are a common way to create matrices, which are often used for mathematical purposes. Take a look at the code block below:
-
->>>
->>> matrix = [[i for i in range(5)] for _ in range(6)]
->>> matrix
-[
-    [0, 1, 2, 3, 4],
-    [0, 1, 2, 3, 4],
-    [0, 1, 2, 3, 4],
-    [0, 1, 2, 3, 4],
-    [0, 1, 2, 3, 4],
-    [0, 1, 2, 3, 4]
-]
-
-
-Take this example, which uses a nested list comprehension to flatten a matrix:
-
->>>
+matrix = [[i for i in range(5)] for _ in range(6)]
+pprint(f"matrix in list comprehension: {matrix}")
+# [
+#     [0, 1, 2, 3, 4],
+#     [0, 1, 2, 3, 4],
+#     [0, 1, 2, 3, 4],
+#     [0, 1, 2, 3, 4],
+#     [0, 1, 2, 3, 4],
+#     [0, 1, 2, 3, 4]
+# ]
+#
+#
+# Take this example, which uses a nested list comprehension to flatten a matrix:
+#
+#
 matrix = [
-...     [0, 0, 0],
-...     [1, 1, 1],
-...     [2, 2, 2],
-... ]
->>> flat = [num for row in matrix for num in row]
->>> flat
-[0, 0, 0, 1, 1, 1, 2, 2, 2]
+    [0, 0, 0],
+    [1, 1, 1],
+    [2, 2, 2],
+]
+flat = [num for row in matrix for num in row]
+print(f"Matrix flattened: {flat}")
+# [0, 0, 0, 1, 1, 1, 2, 2, 2]
+#
+#
+# On the other hand, if you were to use for loops to flatten the same matrix,
+# then your code will be much more straightforward:
+#
+#
+# matrix = [
+#     [0, 0, 0],
+#     [1, 1, 1],
+#     [2, 2, 2],
+# ]
+# flat = []
+# for row in matrix:
+#     for num in row:
+#         flat.append(num)
+#
+# flat
+# [0, 0, 0, 1, 1, 1, 2, 2, 2]
+#
+#
+# sum the squares of the first one-thousand integers, then a list comprehension
+# will solve this problem admirably:
+#
+#
+# sum([i * i for i in range(1000)])
+# 332833500
+#
+#
+# The example below uses a generator:
+#
+#
+# sum(i * i for i in range(1000000000))
+# 333333332833333333500000000
+#
+#
+# map() also operates lazily, meaning memory won’t be an issue if you choose to use it in this case:
+#
+#
+# sum(map(lambda i: i*i, range(1000000000)))
+
+# 333333332833333333500000000
+#
+#
+# You can use timeit to compare the runtime of map(), for loops, and list comprehensions:
+#
+#
 
 
-On the other hand, if you were to use for loops to flatten the same matrix, then your code will be much more straightforward:
-
->>>
->>> matrix = [
-...     [0, 0, 0],
-...     [1, 1, 1],
-...     [2, 2, 2],
-... ]
->>> flat = []
->>> for row in matrix:
-...     for num in row:
-...         flat.append(num)
-...
->>> flat
-[0, 0, 0, 1, 1, 1, 2, 2, 2]
+TAX_RATE = .08
+txns = [random.randrange(100) for _ in range(1000000)]
 
 
-sum the squares of the first one-thousand integers, then a list comprehension will solve this problem admirably:
-
->>>
->>> sum([i * i for i in range(1000)])
-332833500
+def get_price(txn):
+    return txn * (1 + TAX_RATE)
 
 
-The example below uses a generator:
-
->>>
->>> sum(i * i for i in range(1000000000))
-333333332833333333500000000
+def get_prices_with_map():
+    return list(map(get_price, txns))
 
 
-map() also operates lazily, meaning memory won’t be an issue if you choose to use it in this case:
-
->>>
->>> sum(map(lambda i: i*i, range(1000000000)))
-333333332833333333500000000
+def get_prices_with_comprehension():
+    return [get_price(txn) for txn in txns]
 
 
-You can use timeit to compare the runtime of map(), for loops, and list comprehensions:
+def get_prices_with_loop():
+    prices = []
+    for txn in txns:
+        prices.append(get_price(txn))
+    return prices
 
->>>
->>> import random
->>> import timeit
->>> TAX_RATE = .08
->>> txns = [random.randrange(100) for _ in range(100000)]
->>> def get_price(txn):
-...     return txn * (1 + TAX_RATE)
-...
->>> def get_prices_with_map():
-...     return list(map(get_price, txns))
-...
->>> def get_prices_with_comprehension():
-...     return [get_price(txn) for txn in txns]
-...
->>> def get_prices_with_loop():
-...     prices = []
-...     for txn in txns:
-...         prices.append(get_price(txn))
-...     return prices
-...
->>> timeit.timeit(get_prices_with_map, number=100)
-2.0554370979998566
->>> timeit.timeit(get_prices_with_comprehension, number=100)
-2.3982384680002724
->>> timeit.timeit(get_prices_with_loop, number=100)
-3.0531821520007725
+
+#
+
+
+print(f"Timer with map: {timeit.timeit(get_prices_with_map, number=100)}")
+# 2.0554370979998566
+print(f"Timer with comprehension: {timeit.timeit(get_prices_with_comprehension, number=100)}")
+# 2.0554370979998566
+# 2.3982384680002724
+print(f"Timer with loop: {timeit.timeit(get_prices_with_loop, number=100)}")
+# 2.0554370979998566
+# 3.0531821520007725
